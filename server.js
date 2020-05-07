ccc14f7ed817669eb0b1ff5f8ab45afb499e1d34
Zetterman Joachim <joachim.zetterman@scania.com>
2020-05-08 01:50:05 +0200

    Added file

--- server.js
+++ server.js
@@ -2,6 +2,54 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+require('./models/Artist');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(require('./routes'));
+
+app.use(function (err, req, res, next) {
+    res.status(500).send('Something broke!')
+})
+
+app.listen(port, () => console.log(`Mashup API server listening on port ${port}!`))
+
+
+
+
+/*
+
+Post med MBID
+
+Get musicbrainz
+    check relation -> 
+        type = wikidata,  return id from musicbrainz (Q11649)
+        
+        type = wikipedia, return name (Nirvana%20(band))
+
+
+    release-groups: list of albums (MBID, title)
+
+if( wikidata )
+    name = get wikidata(id)    (sitelinks-> enwiki -> title -> Nirvana (band) --> Nirvana%20(band))
+    description = get wikipedia(name)
+    
+if(wikipedia)
+    description = get wikipedia(name)
+
+get coverArt(MBID of albums)
+
+Return to user:
+{
+    "mbid": 
+    "description":
+    "albums": [
+        {
+            "title":
+            "id":
+            "image":
+        }
+    ]
+}
+
+
+
+*/
